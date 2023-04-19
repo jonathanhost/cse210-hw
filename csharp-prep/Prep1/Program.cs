830e2d7bff63f9446ba3508efa1b7b6344f57088
Jonathan de Almeida <jonathan@hostinger.com.br>
2023-04-19 06:34:29 -0300

    activities week1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your Grade?");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter_grade = "";
+        if (grade >= 90 )
+        {
+            letter_grade = "A";
+        }
+        else if (grade >= 80 )
+        {
+            letter_grade = "B";
+        }
+        else if (grade >= 70 )
+        {
+            letter_grade = "C";
+        }
+        else if (grade >= 60 )
+        {
+            letter_grade = "D";
+        }
+        else if (grade <  60 )
+        {
+            letter_grade = "E";;
+        };
+        Console.WriteLine($"Your grade is {letter_grade}!");
+        if (grade < 70 ){
+            Console.WriteLine("You are repproved, please try again");
+        }
     }
 }
