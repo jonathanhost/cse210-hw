67e908c248f52f852999048bee1cad55b3bce5e4
Jonathan de Almeida <jonathan@hostinger.com.br>
2023-06-14 06:01:49 -0300

    shape

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,18 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+    {   List<Shape> shapes = new List<Shape>();
+         Square square = new Square("blue", 10);
+         Rectangle rectangle = new Rectangle("red",10,20);
+         Circle circle = new Circle("red",20);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        foreach(Shape shape in shapes){
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+
+        }
+        
     }
 }
